6ab3dd32b59debe5ad6edb4c4a07cd518e7de568
Alexander Klokov <alexander.klokov@gmail.com>
2023-11-15 15:36:59 -0600

    enchance testing

--- src/sentiment_test.py
+++ src/sentiment_test.py
@@ -19,9 +19,14 @@ model = tf.keras.models.load_model(FILE_NAME_MODEL)
 #evaluate the model
 model.evaluate(X_test_indices, Y_test)
 
-##
+# visualize test results
+X_test_small, Y_test_small = get_dataset_split('train', batch_size=11)
 
-predictions = model.predict(X_test_indices)
-for prediction in predictions:
-    max_index = tf.argmax(prediction)
-    print(X_test_indices[max_index], ':', emotions[max_index])
+X_test_small_indices = tokenizer.texts_to_sequences(X_test)
+X_test_small_indices = pad_sequences(X_test_indices, maxlen=max_len, padding='post')
+
+predictions = model.predict(X_test_small_indices)
+for prediction, sentence, labels in zip(predictions, X_test_small, Y_test_small):
+    max_index_true = tf.argmax(labels)
+    max_index_prediction = tf.argmax(prediction)
+    print(sentence, ':', emotions[max_index_prediction], "(", emotions[max_index_true], ")")
