258e873350cbcc0ee752ab9b0ff01afc0b2256a8
Alexander Klokov <alexander.klokov@gmail.com>
2023-11-15 16:01:05 -0600

    fix typo

--- src/sentiment_test.py
+++ src/sentiment_test.py
@@ -5,7 +5,7 @@ from keras.preprocessing.sequence import pad_sequences
 
 from sentiment_utils import FILE_NAME_MODEL, get_dataset_split, emotions, max_len
 
-X_test, Y_test = get_dataset_split('train', batch_size=2000)
+X_test, Y_test = get_dataset_split('test', batch_size=2000)
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(X_test)
@@ -20,7 +20,7 @@ model = tf.keras.models.load_model(FILE_NAME_MODEL)
 model.evaluate(X_test_indices, Y_test)
 
 # visualize test results
-X_test_small, Y_test_small = get_dataset_split('train', batch_size=11)
+X_test_small, Y_test_small = get_dataset_split('test', batch_size=11)
 
 X_test_small_indices = tokenizer.texts_to_sequences(X_test)
 X_test_small_indices = pad_sequences(X_test_indices, maxlen=max_len, padding='post')
